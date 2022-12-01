b8dc001f3962290a6b298205626f7c002388d512
Benjamin Moore <benjaminjm@gmail.com>
2022-12-01 00:26:10 -0800

    Day 1 Part 2

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,24 @@ fn main() {
     let f = File::open("input-1.txt").unwrap();
     let f = BufReader::new(f);
 
-    let mut max_elf = 0;
+    let mut elfs = Vec::new();
     let mut current_elf = 0;
 
     for line in f.lines() {
         let line = line.unwrap();
         if line == "" {
-            if current_elf > max_elf {
-                max_elf = current_elf;
-            }
+            elfs.push(current_elf);
             current_elf = 0;
         }
         else {
             current_elf += line.parse::<i32>().unwrap();
         }
-
     }
 
-    println!("The elf with the most calories is carrying {} calories", max_elf);
+    elfs.sort();
+    elfs.reverse();
+
+    let top_3: i32 = elfs[..3].iter().sum();
+
+    println!("The top 3 elfs with the most calories are carrying {:?} calories", top_3);
 }
