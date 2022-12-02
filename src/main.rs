1dd2706637a8732713966f88b9c04afafee0c933
Benjamin Moore <benjaminjm@gmail.com>
2022-12-01 21:32:36 -0800

    Day 2

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::io::{BufRead, BufReader};
+use std::collections::HashMap;
 use std::fs::File;
+use std::io::{BufRead, BufReader};
 
 fn main() {
     day_1();
@@ -21,8 +22,7 @@ fn day_1() {
                 elfs.sort();
             }
             current_elf = 0;
-        }
-        else {
+        } else {
             current_elf += line.parse::<i32>().unwrap();
         }
     }
@@ -32,5 +32,41 @@ fn day_1() {
 }
 
 fn day_2() {
+    let f = File::open("input-2.txt").unwrap();
+    let f = BufReader::new(f);
 
+    let mut score_1 = 0;
+    let strategy_values_1 = HashMap::from([
+        ("A X", 4),
+        ("A Y", 8),
+        ("A Z", 3),
+        ("B X", 1),
+        ("B Y", 5),
+        ("B Z", 9),
+        ("C X", 7),
+        ("C Y", 2),
+        ("C Z", 6),
+    ]);
+
+    let mut score_2 = 0;
+    let strategy_values_2 = HashMap::from([
+        ("A X", 3),
+        ("A Y", 4),
+        ("A Z", 8),
+        ("B X", 1),
+        ("B Y", 5),
+        ("B Z", 9),
+        ("C X", 2),
+        ("C Y", 6),
+        ("C Z", 7),
+    ]);
+
+    for line in f.lines() {
+        let line = line.unwrap();
+        score_1 += strategy_values_1.get(&*line).unwrap();
+        score_2 += strategy_values_2.get(&*line).unwrap();
+    }
+
+    println!("Day 2 Part 1: {}", score_1);
+    println!("Day 2 Part 2: {}", score_2);
 }
