fba710023018ca4c309843bdd7d75f47a4fdff8d
Benjamin Moore <benjaminjm@gmail.com>
2022-12-28 21:16:47 -0800

    Mild refactorizing. Kill some hashmaps!

--- src/main.rs
+++ src/main.rs
@@ -1,135 +1,36 @@
-use crate::day1::solve;
-use itertools::Itertools;
+use clap::Parser;
 use regex::Regex;
 use std::cell::RefCell;
 use std::cmp::Ordering;
 use std::collections::{HashMap, HashSet, VecDeque};
 use std::fs::{read_to_string, File};
 use std::io::{BufRead, BufReader};
+use std::path::PathBuf;
 use std::str::FromStr;
+use std::time::Instant;
 
-mod day1;
+pub mod solutions;
+
+#[derive(Parser)]
+struct Cli {
+    #[arg(short, long)]
+    day: Option<u8>,
+
+    #[arg(short, long, value_name = "FILE")]
+    input: Option<PathBuf>,
+}
 
 fn main() {
-    // Common functionality:
-    // parse args
-    // getting the input file
-    // print result with benchmark
-    day1::solve();
-    day_2();
-    day_3();
-    day_4();
-    day_5();
-    day_6();
-    day_7();
-    day_8();
-    day_9();
-    day_10();
-    day_11();
-    day_12();
-    day_13();
-    //day_14();
-    day_15();
-    day_18();
-}
+    let cli = Cli::parse();
+    let start = Instant::now();
 
-fn day_2() {
-    let f = File::open("input-2.txt").unwrap();
-    let f = BufReader::new(f);
-
-    let mut score_1 = 0;
-    let strategy_values_1 = HashMap::from([
-        ("A X", 4),
-        ("A Y", 8),
-        ("A Z", 3),
-        ("B X", 1),
-        ("B Y", 5),
-        ("B Z", 9),
-        ("C X", 7),
-        ("C Y", 2),
-        ("C Z", 6),
-    ]);
-
-    let mut score_2 = 0;
-    let strategy_values_2 = HashMap::from([
-        ("A X", 3),
-        ("A Y", 4),
-        ("A Z", 8),
-        ("B X", 1),
-        ("B Y", 5),
-        ("B Z", 9),
-        ("C X", 2),
-        ("C Y", 6),
-        ("C Z", 7),
-    ]);
-
-    for line in f.lines() {
-        let line = line.unwrap();
-        score_1 += strategy_values_1.get::<str>(&line).unwrap();
-        score_2 += strategy_values_2.get::<str>(&line).unwrap();
+    if let Some(day) = cli.day {
+        solutions::run_one(day);
+    } else {
+        solutions::run_all();
     }
 
-    println!("Day 2 Part 1: {}", score_1);
-    println!("Day 2 Part 2: {}", score_2);
-}
-
-fn day_3() {
-    let f = File::open("input-3.txt").unwrap();
-    let f = BufReader::new(f);
-    let priority_map: HashMap<char, i32> = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        .chars()
-        .zip(0..)
-        .collect();
-
-    let mut priority_total_area = 0;
-
-    for chunk in &f.lines().chunks(3) {
-        let shared_char = chunk
-            .map(|line| line.unwrap().chars().collect::<HashSet<char>>())
-            .reduce(|acc, hs| hs.intersection(&acc).copied().collect())
-            .unwrap()
-            .iter()
-            .next()
-            .unwrap()
-            .clone();
-
-        priority_total_area += priority_map.get(&shared_char).unwrap();
-    }
-
-    println!("Day 3 Part 1: {}", "NOT IMPLEMENTED");
-    println!("Day 3 Part 2: {}", priority_total_area);
-}
-
-fn day_4() {
-    let f = File::open("input-4.txt").unwrap();
-    let f = BufReader::new(f);
-
-    let mut contained = 0;
-    let mut overlap = 0;
-
-    for line in f.lines() {
-        let line = line.unwrap();
-
-        let vals: Vec<u32> = line
-            .split(&['-', ','])
-            .map(|s| s.parse().unwrap())
-            .collect();
-
-        if vals[0] <= vals[2] && vals[1] >= vals[3] {
-            contained += 1;
-            overlap += 1;
-        } else if vals[0] >= vals[2] && vals[1] <= vals[3] {
-            contained += 1;
-            overlap += 1;
-        } else if (vals[0] >= vals[2] && vals[0] <= vals[3])
-            || (vals[1] >= vals[2] && vals[1] <= vals[3])
-        {
-            overlap += 1;
-        }
-    }
-
-    println!("Day 4 Part 1: {}", contained);
-    println!("Day 4 Part 2: {}", overlap);
+    println!("Total runtime: {}μs", start.elapsed().as_micros());
 }
 
 fn day_5() {
